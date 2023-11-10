7932c0f400c35928fe4b482ac550ffb3dbc06441
arian-lrd <97740803+arian-lrd@users.noreply.github.com>
2023-11-09 19:56:14 -0500

    Addition of Signup/login/logout

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,9 @@
 # render and redirection
 from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserCreationForm, LoginForm
+from django.contrib.auth.decorators import login_required
 
 # handling file storage natively
 from django.core.files.storage import FileSystemStorage
@@ -7,6 +11,7 @@ from django.core.files.storage import FileSystemStorage
 # Create your views here.
 
 # upload view; referencing to the html and css for details
+@login_required
 def upload(request):
     # dictionary for data exchange between view and upload.html
     context = {}
@@ -26,5 +31,40 @@ def upload(request):
         # generates key-value pair: key being the url 
         context['url'] = fs.url(name)
     return render(request, 'upload.html', context)
+
+
+
+# Signup page
+def user_signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+# login page
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('upload')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+
+# logout page
+def user_logout(request):
+    logout(request)
+    return redirect('login')
 
 
